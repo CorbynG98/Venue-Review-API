3ab04cb7fbf3172d711ae8d24ad540c6fb928711
Corbyn Greenwood <corbyn.greenwood.98@gmail.com>
2019-03-16 19:37:53 +1300

    Fixed bug causing a crash when checking a given auth token against the database

--- app/Util/authCheck.js
+++ app/Util/authCheck.js
@@ -1,6 +1,7 @@
 const db = require('../../config/db');
 
 exports.checkUserAuth = function(token, done) {
+    if (token == undefined) return done(null);
     db.getPool().query("SELECT * FROM User WHERE auth_token = ?", token, function(err, result) {
         if (err) return done(err);
         if (result == "" || result == null || result == []) return done(null);
