baa01dd1ccc21c110c3b8d8d8c499cacf2bd6032
ctg31 <ctg31@uclive.ac.nz>
2019-03-24 14:28:55 +1300

    Implemented all of the review endpoints

--- app/controllers/reviews.controller.js
+++ app/controllers/reviews.controller.js
@@ -14,17 +14,127 @@ exports.getVenueReviews = function(req, res) {
             res.json("Not Found");
             return
         }
+
+        let newResult = [];
+
+        for (let i = 0; i < result.length; i++) {
+            newResult.push({
+                "reviewAuthor": {
+                    "userId": result[i].review_author_id,
+                    "username": result[i].username
+                },
+                "reviewBody": result[i].reviewBody,
+                "starRating": result[i].starRating,
+                "costRating": result[i].costRating,
+                "timePosted": result[i].timePosted
+            });
+        }
+
         res.status(200);
-        res.json(result);
+        res.json(newResult);
     });
 };
 
 exports.getUserReviews = function(req, res) {
-    res.status(200);
-    res.json("OK");
+    let user_id = req.params.id;
+
+    let values = [
+        [user_id]
+    ];
+
+    Reviews.getUserReviews(values, function(result) {
+        if (result == null) {
+            res.status(404);
+            res.json("Not Found");
+            return
+        }
+
+        let newResult = [];
+
+        for (let i = 0; i < result.length; i++) {
+            newResult.push({
+                "reviewAuthor": {
+                    "userId": result[i].review_author_id,
+                    "username": result[i].username
+                },
+                "reviewBody": result[i].reviewBody,
+                "starRating": result[i].starRating,
+                "costRating": result[i].costRating,
+                "timePosted": result[i].timePosted,
+                "venue": {
+                    "venueId": result[i].venueId,
+                    "venueName": result[i].venueName,
+                    "categoryName": result[i].categoryName,
+                    "city": result[i].city,
+                    "shortDescription": result[i].shortDescription,
+                    "primaryPhoto": result[i].primaryPhoto
+                }
+            });
+        }
+
+        res.status(200);
+        res.json(newResult);
+    });
 };
 
 exports.create = function(req, res) {
-    res.status(200);
-    res.json("OK");
+    let user_data = {
+        "venue_id": req.params.id,
+        "review_body": req.body.reviewBody,
+        "star_rating": req.body.starRating,
+        "cost_rating": req.body.costRating
+    };
+
+    authCheck.checkUserAuth(req.headers["x-authorization"], function(authResult) {
+        if (authResult == null || authResult == "" || authResult == []) {
+            res.status(401);
+            res.json("Unauthorized");
+            return;
+        }
+        Reviews.checkReviewer(authResult[0].user_id, function(result) {
+            if (result.length != 0) {
+                res.status(403);
+                res.json("Forbidden");
+                return;
+            }
+            user_data.review_author_id = authResult[0].user_id;
+
+            for (let item in user_data) {
+                if (user_data[item] == undefined || user_data[item] == "") {
+                    res.status(400);
+                    res.json("Bad Request");
+                    return;
+                }
+                if (user_data["cost_rating"] % 1 != 0 || user_data["cost_rating"] < 0 || user_data["cost_rating"] > 4) {
+                    res.status(400);
+                    res.json("Bad Request");
+                    return;
+                }
+                if (user_data["star_rating"] % 1 != 0 || user_data["star_rating"] < 0 || user_data["star_rating"] > 5) {
+                    res.status(400);
+                    res.json("Bad Request");
+                    return;
+                }
+            }
+
+            let values = [
+                [user_data["venue_id"],
+                user_data["review_body"],
+                user_data["star_rating"],
+                user_data["cost_rating"],
+                user_data["review_author_id"]]
+            ];
+
+            Reviews.insert(user_data["venue_id"], values, function(result) {
+                if (result == null) {
+                    res.status(404);
+                    res.json("Not Found");
+                    return;
+                }
+                res.status(201);
+                res.json("Created");
+                return;
+            });
+        });
+    })
 };
--- app/models/reviews.model.js
+++ app/models/reviews.model.js
@@ -1,19 +1,48 @@
 const db = require('../../config/db');
 
 exports.getVenueReviews = function(values, done) {
-    db.getPool().query("SELECT * FROM Review WHERE reviewed_venue_id = ?", values, function(rows) {
+    db.getPool().query("SELECT * FROM Review WHERE reviewed_venue_id = ?", values, function(err, rows) {
         if (rows == "" || rows == []) return done(null);
-        db.getPool().query("SELECT review_author_id as reviewAuthor, review_body as reviewBody, star_rating as starRating, cost_rating as costRating, time_posted as timePosted" +
-            " FROM Review WHERE reviewed_venue_id = ? ORDER BY timePosted", values, function(rows) {
+        db.getPool().query("SELECT review_author_id, username, review_body as reviewBody, star_rating as starRating, cost_rating as costRating, time_posted as timePosted" +
+            " FROM Review JOIN User ON Review.review_author_id = user_id WHERE reviewed_venue_id = ? ORDER BY timePosted DESC", values, function(err, rows) {
             return done(rows);
         });
     });
 };
 
-exports.insert = function(done) {
-    return done(null);
+exports.checkReviewer = function(user_id, done) {
+    db.getPool().query("SELECT * FROM Venue JOIN Review ON Review.reviewed_venue_id = venue_id WHERE Venue.admin_id = ? OR Review.review_author_id = ?", [user_id, user_id], function(err, rows) {
+        if (err) return done(err);
+        return done(rows);
+    });
 };
 
-exports.getUserReviews = function(done) {
-    return done(null);
+exports.insert = function(venue_id, values, done) {
+    db.getPool().query("SELECT * FROM Venue WHERE venue_id = ?", venue_id, function(err, rows) {
+        if (err) return done(err);
+        if (rows == "" || rows == null || rows == []) return done(null);
+        db.getPool().query("INSERT INTO Review (reviewed_venue_id, review_body, star_rating, cost_rating, review_author_id) VALUES (?)", values, function(err, rows) {
+            if (err) return done(err);
+            return done(rows);
+        });
+    });
+};
+
+exports.getUserReviews = function(values, done) {
+    db.getPool().query("SELECT * FROM User WHERE user_id = ?", values, function(err, rows) {
+        if (rows == "" || rows == []) return done(null);
+        db.getPool().query("SELECT user_id, username, review_body as reviewBody, star_rating as starRating, cost_rating as costRating, time_posted as timePosted, Venue.venue_id as venueId, venue_name as venueName, " +
+                "(SELECT category_name " +
+                "FROM VenueCategory " +
+                "WHERE VenueCategory.category_id = Venue.category_id) as categoryName, " +
+            "city, short_description as shortDescription, " +
+            "(SELECT photo_filename FROM VenuePhoto WHERE is_primary = 1 AND VenuePhoto.venue_id = Venue.venue_id) as primaryPhoto " +
+            "FROM User " +
+            "LEFT JOIN Review ON Review.review_author_id = user_id " +
+            "LEFT JOIN Venue ON Venue.venue_id = Review.reviewed_venue_id " +
+            "LEFT JOIN VenuePhoto ON VenuePhoto.venue_id = Venue.venue_id " +
+            "WHERE User.user_id = ? ", values, function(err, rows) {
+            return done(rows);
+        });
+    });
 };
